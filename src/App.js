6972665159242cd3d90edf97ebc753b00688a41d
Rebenkov Aleksey <rebenkovaleksey@yandex.ru>
2020-07-31 14:19:06 +0700

    Chart's demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Bar } from 'react-chartjs-2';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Bar
+        data={{
+          labels: ['Первый квартал', 'Последний квартал'],
+          datasets: [{
+            label: 'Доступно к вычету',
+            data: [{x: 0, y: 10}, {x: 0, y: 1}],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(255, 99, 132, 0.8)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 1
+          },
+          {
+            label: 'Начислено',
+            data: [{x: 1, y: 3}, {x: 1, y: 4}],
+            backgroundColor: [
+                'rgba(153, 102, 255, 0.8)',
+                'rgba(153, 102, 255, 0.8)'
+            ],
+            borderColor: [
+              'rgba(153, 102, 255, 1)',
+              'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
+          label: 'К уплате',
+          data: [{x: 1, y: 8}, {x: 1, y: 5}],
+          backgroundColor: [
+              'rgba(255, 159, 64, 0.8)',
+              'rgba(255, 159, 64, 0.8)'
+          ],
+          borderColor: [
+            'rgba(255, 159, 64, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+        }}
+        width={100}
+        height={50}
+        options={{
+          onClick: function(e) {
+            // так можно определить на какой из секторов кликнули
+            console.log(this.scales[`x-axis-0`].getValueForPixel(e.x));
+            console.log(this.scales[`y-axis-0`].getValueForPixel(e.y));
+          },
+          scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+          }
+        }}
+      />
     </div>
   );
 }
